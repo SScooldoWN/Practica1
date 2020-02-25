c54088ed4f2dc1393e1c8005ac5cb9db06a9291a
SScooldoWN <60805853+SScooldoWN@users.noreply.github.com>
2020-02-24 23:43:59 -0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -3,27 +3,90 @@ import 'package:practica_integradora_uno/home/home.dart';
 import 'package:practica_integradora_uno/log_in.dart';
 import 'package:practica_integradora_uno/utils/constants.dart';
 
-
 class SignIn extends StatefulWidget {
-  SignIn({Key key}) : super(key: key);
-
   @override
   _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
- var valor = false;
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: blueS,
-        body: Container(
-            child: Column(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
               children: <Widget>[
-               Center( child: Image.asset("lib/assets/image/cupping.png",
-           height: 150,), 
+                Image.asset("lib/assets/image/cupping.png",
+           height: 150,
         ),
-              ]
-            )));
+                SizedBox(height: 16.0),
+                Text(
+                  ' ',
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              ],
+            ),
+            SizedBox(height: 12.0),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Nombre Completo',
+                ),
+              ),
+              
+            SizedBox(height: 12.0),
+               TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+              ),
+            ButtonBar(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Entrar'),
+                  elevation: 8.0,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                  ),
+                  onPressed: () async {
+                    Navigator.push(context,
+              MaterialPageRoute(builder: (_) => Home(title: APP_TITLE)
+              ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 12.0),
+              Text("Olvidaste tu contrasena?"),
+            SizedBox(height: 20.0),
+              Text("No tienes cuenta?"),
+            MaterialButton(
+            child: Text(
+              "Registrate",
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: white,
+                fontSize: 15,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => LogIn()));
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
